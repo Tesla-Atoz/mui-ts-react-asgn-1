9f0d81c4afbd9e6877bc383dfc0d8eff292d7ead
Tesla-Atoz <teslaatozinspires7@gmail.com>
2021-09-16 22:27:14 +0530

    built detailform-plain detailcard-plain using material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DetailCard from "./components/DetailCard/DetailCard";
+import DetailForm from "./components/DetailForm/DetailForm";
+import { CssBaseline } from "@material-ui/core";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <DetailCard />
+      <DetailForm />
     </div>
   );
 }
